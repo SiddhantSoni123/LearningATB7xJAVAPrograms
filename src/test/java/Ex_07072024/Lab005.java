c5f9bc3dd55ee7f8eb0e2634ec040dbc3b69ef63
Siddhant Soni <siddhantsoni846@gmail.com>
2024-07-13 17:12:39 +0530

    1st Commit

---
+++ src/test/java/Ex_07072024/Lab005.java
@@ -0,0 +1,17 @@
+package Ex_07072024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(10/0);
+    }
+}
+/**
+  4
+  0
+  1
+  Exception in thread "main" java.lang.ArithmeticException: / by zero
+  at Ex_07072024.Lab005.main(Lab005.java:8)
+**/
