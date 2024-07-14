61b9d3b76a7313e5d2a6e86585b01536fca6ca93
Siddhant Soni <siddhantsoni846@gmail.com>
2024-07-14 16:24:53 +0530

    1st Commit

---
+++ src/test/java/Ex_14072024/Lab050.java
@@ -0,0 +1,23 @@
+package Ex_14072024;
+
+public class Lab050 {
+    public static void main(String[] args) {
+        int age = 65;
+        int dzire = -1;
+        //data_type, variable_name(identifier) = variable_value(literals)
+        //unary = only 1 operand & 1 operator 
+        //age - operand, value - unary plus, = -> operator
+        //dzire - operand, value - unary minus, = -> operator
+
+
+        //Binary Operator
+        int a = 10;
+        int b = 23;
+        int sum = a + b;
+        // Binary - Arithmetic +,-,*,/....
+        // Two operand
+        // a, b -> operand , + operator
+
+        // Ternary Operator a,b,c - operand
+    }
+}
